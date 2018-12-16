a65a2295fbfc72687f778690f5558cd31a26dcef
Navon Francis <navonfrancis@gmail.com>
2018-12-16 13:29:05 -0500

    medium - kth largest element in an array.

---
+++ medium/kthLargestElement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x, y) -> y - x);
+        
+        for(int num : nums) {
+            pq.offer(num);
+        }
+        
+        for(int i = 0; i < k-1; i++) {
+            pq.poll();
+        }
+        
+        return pq.peek();
+    }
+}
