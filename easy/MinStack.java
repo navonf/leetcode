ac673c102a01e2cb13e97b4abfb92ec82dee206b
Navon Francis <navonfrancis@gmail.com>
2018-12-18 20:38:38 -0500

    easy - min stack correct.

--- easy/MinStack.java
+++ easy/MinStack.java
@@ -1,54 +1,40 @@
-/* Navon Francis, 9/25/18
- * This code runs in O(1) time complexity,
- * O(n) space complexity.
- */
 class MinStack {
-    ArrayList<Integer> data;
-    ArrayList<Integer> min;
+    public ArrayDeque<Integer> stack;
+    public ArrayDeque<Integer> min;
     /** initialize your data structure here. */
     public MinStack() {
-        data = new ArrayList<Integer>();
-        min = new ArrayList<Integer>();
-        min.add(Integer.MAX_VALUE);
+        stack = new ArrayDeque<>();
+        min = new ArrayDeque<>();
     }
-
+    
     public void push(int x) {
-        data.add(x);
-        // Check the top of min, if it's bigger, add in the new min.
-        if(min.get(min.size()-1) > x) {
-            min.add(x);
+        stack.push(x);
+        
+        if(min.isEmpty()) {
+            min.push(x);
         }
-        // If the current min is smaller, just add that same min.
+        // then compare in the minimum stack.
         else {
-            min.add(min.get(min.size()-1));
+            if(min.peek() < x) {
+                min.push(min.peek());
+            }
+            else {
+                min.push(x);
+            }
         }
-        // System.out.println(data);
     }
-
+    
     public void pop() {
-        // System.out.println("Before the pop" + data);
-        // System.out.println("Index of the pop" + (data.size()-1));
-        // Don't pop if ArrayList is empty!
-        if(!data.isEmpty()) {
-            // Remove the last thing in the list.
-            // ex:
-            // [1, 2, 3, ..nothing else..]
-            // size = 3, data.remove(2);
-            // [1, 2, ...nothing else..]
-            data.remove(data.size()-1);
-            min.remove(min.size()-1);
-        }
-        // System.out.println("After the pop " + data);
+        stack.pop();
+        min.pop();
     }
-
+    
     public int top() {
-        // Return dat last ele.
-        return data.get(data.size()-1);
+        return stack.peek();
     }
-
+    
     public int getMin() {
-        // System.out.println("GET MIN: " + data);
-        return min.get(min.size()-1);
+        return min.peek();
     }
 }
 
