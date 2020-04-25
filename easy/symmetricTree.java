661adfcea239295aaf505ee3ed8a258e295b8b75
Navon Francis <navonfrancis@gmail.com>
2020-04-25 18:02:15 -0400

    redid symmetric tree, clearer logic

--- easy/symmetricTree.java
+++ easy/symmetricTree.java
@@ -9,15 +9,38 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null) return true;
-        return check(root.left, root.right);
+        if (root == null) {
+            return true;
+        }
+        
+        return dfs(root.left, root.right);
     }
     
-    public boolean check(TreeNode left, TreeNode right) {
-        if(left == null && right == null) return true;
-        if(left == null && right != null || right == null && left != null) return false;
-        if(left.val != right.val) return false;
+    // falses
+    // node1.val != node2.val
+    // node1 == null && node2 != null
+    // node1 =! null && node2 == null
+    
+    // trues
+    // node1.val == node2.val
+    // node1.left == node2.right
+    // node1.right == node2.left
+    
+    private boolean dfs(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
         
-        return check(left.right, right.left) && check(left.left, right.right);
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        
+        if (node1.val != node2.val) {
+            return false;
+        }
+        
+        return node1.val == node2.val &&
+            dfs(node1.left, node2.right) &&
+            dfs(node1.right, node2.left);
     }
 }
