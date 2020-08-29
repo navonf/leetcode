3cbee708a5cbffc4d53d0d0849a375c614be9ac5
Navon Francis <navonfrancis@gmail.com>
2020-08-29 17:03:13 -0400

    medium - redid top k frequent words, practicing custom comprator

--- medium/topKFrequentWords.java
+++ medium/topKFrequentWords.java
@@ -1,80 +1,49 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        Map<String, Word> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        PriorityQueue<Word> pq = new PriorityQueue<>(new Comparator<Word>() {
-            // the compare method returns an positive or negative integer based on
-            // these 2 elements. return 1 means the current order is bad, swap. return -1
-            // mean the current order is good, dont swap. if its equal to each other, usually
-            // return 0 but in our case we will then sort by lex order using String.compareTo().
-            @Override
-            public int compare(Word w1, Word w2) {
-                // ** w1 freq greater than w2 freq, keep the order. **
-                // given some order of element 1, and element 2,
-                // if the value of element 1 is greater than element 2, return a -1
-                // which is a negative integer less than 0, don't swap! because this
-                // is what we want, the higher frequency to be at the front of our PQ.
-                if(w1.freq > w2.freq) {
-                    return -1;
-                }
-                // ** w1 freq lesser than w2 freq, swap the order. **
-                // given some order of element 1, and element 2,
-                // if the value of element 1 is lesser than element 2, return a 1
-                // which is a positive integer greater than 0, to swap them. because
-                // we want to put the one with a higher frequency.
-                else if(w1.freq < w2.freq) {
-                    return 1;
-                }
-                // ** Strings are equal freq, return lexicographic order. **
-                else {
-                    int w1ComapredToW2 = w1.name.compareTo(w2.name);
-                    if(w1ComapredToW2 <= 0) {
-                        return -1;
+        HashMap<String, Integer> map = new HashMap<>();
+        PriorityQueue<Word> pq = new PriorityQueue<>(
+            words.length, 
+            new Comparator<Word>() {
+                @Override
+                public int compare(Word w1, Word w2) {
+                    if (w2.freq == w1.freq) {
+                        return w1.content.compareTo(w2.content);
                     }
-                    else {
-                        return 1;
-                    }
+                    
+                    return Integer.compare(w2.freq, w1.freq);
                 }
             }
-        });
+        );
+        List<String> topK = new LinkedList<>();
         
-        // cache in map.
-        for(String w : words) {
-            if(map.containsKey(w)) {
-                Word word = map.get(w);
-                word.freq++;
-                map.put(w, word);
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                int freq = map.get(word);
+                map.put(word, ++freq);
             }
             else {
-                map.put(w, new Word(w));
+                map.put(word, 1);
             }
         }
         
-        // load into pq.
-        map.forEach((name, word) -> {
-           pq.offer(word); 
+        map.forEach((word, freq) -> {
+            pq.offer(new Word(word, freq));
         });
         
-        // empty for k elements.
-        int i = 0;
-        while(i < k) {
-            Word word = pq.poll();
-            list.add(word.name);
-            i++;
+        for (int i = 0; i < k; i++) {
+            topK.add(pq.poll().content);
         }
         
-        return list;
+        return topK;
     }
-    
-    
 }
 
 class Word {
-    public String name;
+    public String content;
     public int freq;
     
-    Word(String n) {
-        this.name = n;
-        this.freq = 1;
+    public Word(String w, int f) {
+        this.content = w;
+        this.freq = f;
     }
 }
