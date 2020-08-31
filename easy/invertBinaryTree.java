3044861b78894f4460a6157e9c351810f915d10e
Navon Francis <navonfrancis@gmail.com>
2020-08-31 19:30:39 -0400

    easy - redid invert binary tree. my answer was exactly the same, added comments.

--- easy/invertBinaryTree.java
+++ easy/invertBinaryTree.java
@@ -15,20 +15,24 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        TreeNode ret = root;
+        // start traversal.
         invert(root);
-        return ret;
+        return root;
     }
     
     public void invert(TreeNode root) {
+        // we end when the function gets a root that is null.
         if (root == null) {
             return;
         }
         
+        // once we realize that when we swap nodes, all the sub children
+        // are also swapped, we can invert the tree easily.
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
         
+        // continue to invert down the tree.
         invert(root.left);
         invert(root.right);
     }
